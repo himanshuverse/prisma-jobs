79adbfcdf1421aec322dba99debf50c1b9091a22
Himanshu Mittal <himanshu1210mittal@gmail.com>
2026-07-27 23:32:05 +0530

    feat: update User model and seed data with new fields

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
-import express ,{ Request , Response} from "express"
+import express ,{ Request ,  Response} from "express"
+import  {PrismaClient} from "@prisma/client"
 
 const app =express()
+const prisma = new PrismaClient()
 
 app.use(express.json())
 
@@ -9,6 +11,15 @@ app.get("/",(req:Request,res:Response)=>{
     res.send("hello ")
 
 })
+
+app.get("/users",async (_,res:Response)=>{
+    const users = await prisma.user.findUnique({
+        where:{id :3}
+    })
+    res.json(users)
+
+})
+
 
 app.listen(4000,()=>{
     console.log("server is listening at port 4000")
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -5,8 +5,62 @@ async function seed() {
     await prisma.user.createMany(
         {
             data:[
-                {name:"alex",email:"alex@example.com"},
-                {name:"slice",email:"slice@example.com"}
+      {
+        name: "Emily Johnson",
+        email: "emily@example.com",
+        age: 29,
+        isMarried: true,
+        nationality: "American",
+      },
+      {
+        name: "Sophia Brown",
+        email: "sophia@example.com",
+        age: 31,
+        isMarried: true,
+        nationality: "Canadian",
+      },
+      {
+        name: "Liam Wilson",
+        email: "liam@example.com",
+        age: 27,
+        isMarried: false,
+        nationality: "Australian",
+      },
+      {
+        name: "Noah Martinez",
+        email: "noah@example.com",
+        age: 35,
+        isMarried: true,
+        nationality: "Mexican",
+      },
+      {
+        name: "Olivia Davis",
+        email: "olivia@example.com",
+        age: 22,
+        isMarried: false,
+        nationality: "British",
+      },
+      {
+        name: "Ethan Anderson",
+        email: "ethan@example.com",
+        age: 30,
+        isMarried: true,
+        nationality: "American",
+      },
+      {
+        name: "Ava Thomas",
+        email: "ava@example.com",
+        age: 26,
+        isMarried: false,
+        nationality: "French",
+      },
+      {
+        name: "James Taylor",
+        email: "james@example.com",
+        age: 40,
+        isMarried: true,
+        nationality: "German",
+      },
            ]
         }
     )
